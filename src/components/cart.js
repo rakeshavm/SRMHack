f836491e506c48632365b398dd65799637c18bd7
Kanishq <36290185+Stalemate99@users.noreply.github.com>
2019-08-31 08:38:02 +0530

    Add files via upload

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './cart.css'
+import Product from './Product';
 
 class Cart extends React.Component {
     render = () => {
-        return(
-            <div>
-            <div className='cartdiv'> 
-            </div>
+        return (
+            <div className="ui middle aligned center aligned grid">
+                <div className="column" style={{width:'100vw',height:'100vh'}}>
+                    <h2 className="header">CART</h2>
+                    <div className="ui segments">
+                        <h3 className="header">Your Order</h3>
+                        <div className="ui segment">
+                            <Product data={{category:'Beauty',name:'Shampoo',price:'40',qty:'10'}}/>
+                        </div>
+                        <div className="ui segment">
+                            <button className="ui button">
+                                Place Order
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
